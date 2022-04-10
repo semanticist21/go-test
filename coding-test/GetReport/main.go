1b61d0ea5685a62526e1421d463b3cc7f0e7c06a
semanticist21 <92710478+semanticist21@users.noreply.github.com>
2022-04-10 11:40:18 +0900

    33

---
+++ coding-test/GetReport/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "hash/adler32"
+
+func main() {
+	id_list := []string{"muzi", "frodo", "apeach", "neo"}
+	report := []string{"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"}
+	k := 2
+	getSolution(id_list, report, k)
+}
+
+func getSolution(id_list []string, report []string, k int) []int {
+	for _, val := range report {
+		
+	}
+
+	return []int{}
+}
+
+func returnSolution 
